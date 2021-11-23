37299e5752b40829f4a325af049fa4db975f75e9
rikonano <galoisnicolas@gmail.com>
2021-11-23 14:00:10 +0100

    heroku CD

---
+++ covid_alert/src/main/java/api/database/covid_alert/CovidAlertApplication.java
@@ -0,0 +1,12 @@
+package api.database.covid_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidAlertApplication.class, args);
+    }
+}
